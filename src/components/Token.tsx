499060b882cc7a2b7e48b43de925c60f5bf1497e
Ruslan Ivanov <rusivary@gmail.com>
2024-07-06 12:35:14 +0400

    added monospace font in Token

--- src/components/Token.tsx
+++ src/components/Token.tsx
@@ -6,25 +6,47 @@ interface ITokenProps {
 }
 
 export const Token = ({ token, showToken }: ITokenProps) => {
+  const maskedTokenLength = token.length > 4 ? token.length - 4 : 0
+  const maskedToken = showToken
+    ? token.slice(0, -4)
+    : "*".repeat(maskedTokenLength)
+
   return (
     <TokenContainer>
-      <BlurredToken showToken={showToken}>{token.slice(0, -4)}</BlurredToken>
-      <LastFourDigits showToken={showToken}>{token.slice(-4)}</LastFourDigits>
+      <InvisibleToken>{token}</InvisibleToken>
+      <VisibleTokenContainer>
+        <BlurredToken $showToken={showToken}>{maskedToken}</BlurredToken>
+        <LastFourDigits>{token.slice(-4)}</LastFourDigits>
+      </VisibleTokenContainer>
     </TokenContainer>
   )
 }
 
 const TokenContainer = styled.div`
   display: flex;
+  position: relative;
+  font-family: "Courier New", Courier, monospace;
 `
 
-const BlurredToken = styled.span<{ showToken: boolean }>`
-  filter: ${({ showToken }) => (showToken ? "none" : "blur(10px)")};
+const InvisibleToken = styled.span`
+  visibility: hidden;
+  position: absolute;
+  font-family: "Courier New", Courier, monospace;
+`
+
+const VisibleTokenContainer = styled.div`
+  display: flex;
+`
+
+const BlurredToken = styled.span<{ $showToken: boolean }>`
+  filter: ${({ $showToken }) => ($showToken ? "none" : "blur(10px)")};
   transition: filter 0.3s ease;
+  font-family: "Courier New", Courier, monospace;
 `
 
-const LastFourDigits = styled.span<{ showToken: boolean }>`
+const LastFourDigits = styled.span`
   filter: none;
   opacity: 1;
   transition: opacity 0.3s ease;
+  font-family: "Courier New", Courier, monospace;
 `
